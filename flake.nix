19030fca9e1d4559a23e63a92558ed82129547fd
Darek D <darekduda21@gmail.com>
2023-12-03 00:47:34 +0100

    Initialize Rust project defaults on NixOS

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "Small exercises to get you used to reading and writing Rust code";
+
+  inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        cargoBuildInputs = with pkgs; lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.CoreServices
+        ];
+
+        aoc-2023 =
+          pkgs.rustPlatform.buildRustPackage {
+            name = "aoc-2023";
+            version = "0.1.0";
+
+            buildInputs = cargoBuildInputs;
+            nativeBuildInputs = [pkgs.git];
+
+            src = with pkgs.lib; cleanSourceWith {
+              src = self;
+              # a function that returns a bool determining if the path should be included in the cleaned source
+              filter = path: type:
+                let
+                  # filename
+                  baseName = builtins.baseNameOf (toString path);
+                  # path from root directory
+                  path' = builtins.replaceStrings [ "${self}/" ] [ "" ] path;
+                  # checks if path is in the directory
+                  inDirectory = directory: hasPrefix directory path';
+                in
+                inDirectory "src" ||
+                inDirectory "tests" ||
+                hasPrefix "Cargo" baseName ||
+                baseName == "info.toml";
+            };
+
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+      in
+      {
+        devShell = pkgs.mkShell {
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            clippy
+          ] ++ cargoBuildInputs;
+        };
+        apps = let
+          aoc-2023-app = {
+            type = "app";
+            program = "${aoc-2023}/bin/aoc-2023";
+          };
+        in {
+          default = aoc-2023-app;
+          aoc-2023 = aoc-2023-app;
+        };
+        packages = {
+          inherit aoc-2023;
+          default = aoc-2023;
+        };
+      });
+}
