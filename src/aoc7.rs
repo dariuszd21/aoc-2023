c6da48599218b89635d47d6a9c38d62ab6a985ea
Darek D <darekduda21@gmail.com>
2023-12-17 13:22:06 +0100

    AOC7: Solve task 2

--- src/aoc7.rs
+++ src/aoc7.rs
@@ -13,12 +13,10 @@ enum HandType {
 }
 
 impl fmt::Display for HandType {
-
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
-
 
 fn parse_card_label(label: char) -> u8 {
     match label {
@@ -95,6 +93,100 @@ fn parse_card(line: &str) -> Option<(HandType, (u8, u8, u8, u8, u8), u64)> {
     None
 }
 
+fn parse_card_label_with_joker_lowest(label: char) -> u8 {
+    match label {
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        '9' => 8,
+        'T' => 9,
+        'J' => 0,
+        'Q' => 11,
+        'K' => 12,
+        'A' => 13,
+        _ => 1,
+    }
+}
+
+fn parse_card_with_jokers(line: &str) -> Option<(HandType, (u8, u8, u8, u8, u8), u64)> {
+    let mut values: HashMap<char, u8> = HashMap::new();
+    let splitted_line: Vec<_> = line.split(" ").collect();
+    if splitted_line.len() == 2 {
+        let (hand, rank) = (splitted_line[0], splitted_line[1]);
+        let rank_val = match rank.parse::<u64>() {
+            Ok(val) => val,
+            Err(_) => 0,
+        };
+
+        for c in hand.chars() {
+            let current_val = match values.get(&c) {
+                Some(v) => *v,
+                None => 0,
+            };
+            values.insert(c, current_val + 1);
+        }
+
+        let number_of_jokers = match values.remove(&'J') {
+            Some(v) => v,
+            None => 0,
+        };
+
+        let (mut card_with_maximum_values, mut amount) = ('X', 0);
+        for (c, v) in &values {
+            if *v > amount {
+                card_with_maximum_values = *c;
+                amount = *v;
+            }
+        }
+     
+        values.insert(card_with_maximum_values, amount+number_of_jokers);
+        println!("Card with maximum values {} ({})", card_with_maximum_values, amount);
+
+        let is_four_in_hand = values.values().filter(|v| **v == 4).count() == 1;
+        let is_three_in_hand = values.values().filter(|v| **v == 3).count() == 1;
+        let number_of_twos = values.values().filter(|v| **v == 2).count();
+        let hand_type: HandType;
+        if values.len() == 1 {
+            hand_type = HandType::FiveOfKind;
+        } else if values.len() == 5 {
+            hand_type = HandType::HighCard;
+        } else if is_four_in_hand {
+            hand_type = HandType::FourOfKind;
+        } else if is_three_in_hand {
+            hand_type = match number_of_twos {
+                0 => HandType::ThreeOfKind,
+                _ => HandType::FullHouse,
+            };
+        } else {
+            hand_type = match number_of_twos {
+                2 => HandType::TwoPairs,
+                _ => HandType::OnePair,
+            };
+        }
+
+        let parsed_values: Vec<_> = hand
+            .chars()
+            .map(|v| parse_card_label_with_joker_lowest(v))
+            .collect();
+
+        return Some((
+            hand_type,
+            (
+                parsed_values[0],
+                parsed_values[1],
+                parsed_values[2],
+                parsed_values[3],
+                parsed_values[4],
+            ),
+            rank_val,
+        ));
+    }
+    None
+}
+
 pub fn day07_task01() {
     let input_filepath = match std::env::current_dir() {
         Ok(filepath) => filepath.join("input_d07_t01"),
@@ -111,7 +203,10 @@ pub fn day07_task01() {
     for line in file_content.split("\n") {
         println!("{}", line);
         if let Some((hand_type, values, rank_val)) = parse_card(line) {
-            println!("HandType: {}, rank: {}, values: {}", hand_type, rank_val, values.0);
+            println!(
+                "HandType: {}, rank: {}, values: {}",
+                hand_type, rank_val, values.0
+            );
             all_cards.push((hand_type, values, rank_val));
         }
     }
@@ -121,8 +216,41 @@ pub fn day07_task01() {
     for (i, (hand_type, values, rank_val)) in all_cards.iter().enumerate() {
         let val: u64 = i as u64 + 1;
         println!("{} {} {}", val, hand_type, values.0);
-        total_winnings += val*rank_val;
+        total_winnings += val * rank_val;
     }
     println!("Total winnings {}", total_winnings);
+}
 
+pub fn day07_task02() {
+    let input_filepath = match std::env::current_dir() {
+        Ok(filepath) => filepath.join("input_d07_t01"),
+        //Ok(filepath) => filepath.join("input_d07_test"),
+        Err(_) => panic!("Cannot find current directory"),
+    };
+
+    println!("Input filepath: {}", input_filepath.display());
+
+    let file_content = fs::read_to_string(input_filepath).expect("File could not be loaded");
+
+    let mut all_cards: Vec<_> = Vec::new();
+
+    for line in file_content.split("\n") {
+        println!("{}", line);
+        if let Some((hand_type, values, rank_val)) = parse_card_with_jokers(line) {
+            println!(
+                "HandType: {}, rank: {}, values: {}",
+                hand_type, rank_val, values.0
+            );
+            all_cards.push((hand_type, values, rank_val));
+        }
+    }
+
+    all_cards.sort();
+    let mut total_winnings = 0;
+    for (i, (hand_type, values, rank_val)) in all_cards.iter().enumerate() {
+        let val: u64 = i as u64 + 1;
+        println!("{} {} {}", val, hand_type, values.0);
+        total_winnings += val * rank_val;
+    }
+    println!("Total winnings {}", total_winnings);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod aoc7;
 
 fn main() {
-    aoc7::day07_task01();
+    //aoc7::day07_task01();
+    aoc7::day07_task02();
 }
