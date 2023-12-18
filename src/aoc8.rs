bc9c6dd97d6027473108d344817c4909e081de7b
Darek D <darekduda21@gmail.com>
2023-12-18 22:47:39 +0100

    AOC8: Try solving part 2 with bruteforce

--- src/aoc8.rs
+++ src/aoc8.rs
@@ -1,4 +1,4 @@
-use std::{fs, collections::HashMap};
+use std::{collections::HashMap, fs};
 
 enum Direction {
     Right,
@@ -14,11 +14,11 @@ fn parse_instructions(line: &str) -> Vec<Direction> {
             _ => (),
         }
     }
-    
+
     directions
 }
 
-fn add_crossroad_to_map(map: &mut HashMap<String, (String,String)>, line: &str) {
+fn add_crossroad_to_map(map: &mut HashMap<String, (String, String)>, line: &str) {
     let splitted_line: Vec<_> = line.split(" = ").collect();
     if splitted_line.len() == 2 {
         let key = splitted_line[0].to_string();
@@ -26,12 +26,14 @@ fn add_crossroad_to_map(map: &mut HashMap<String, (String,String)>, line: &str) 
         let splitted_values: Vec<_> = splitted_line[1].split(", ").collect();
 
         if splitted_values.len() == 2 {
-            let (left, right) = (splitted_values[0].strip_prefix("(").unwrap().to_string(), splitted_values[1].strip_suffix(")").unwrap().to_string());
-            
+            let (left, right) = (
+                splitted_values[0].strip_prefix("(").unwrap().to_string(),
+                splitted_values[1].strip_suffix(")").unwrap().to_string(),
+            );
+
             map.insert(key, (left, right));
         }
     }
-
 }
 
 pub fn day08_task01() {
@@ -92,15 +94,85 @@ pub fn day08_task01() {
                         }
                         None => (),
                     }
-                
                 }
-                
             }
         }
 
         println!("Find finish in {} steps", steps);
-        
+    }
+}
+
+pub fn day08_task02() {
+    let input_filepath = match std::env::current_dir() {
+        Ok(filepath) => filepath.join("input_d08_t01"),
+        //Ok(filepath) => filepath.join("input_d08_test3"),
+        Err(_) => panic!("Cannot find current directory"),
+    };
+
+    println!("Input filepath: {}", input_filepath.display());
+
+    let file_content = fs::read_to_string(input_filepath).expect("File could not be loaded");
+    let mut directions = Vec::new();
+
+    let mut map: HashMap<_, _> = HashMap::new();
+
+    for (idx, line) in file_content.split("\n").enumerate() {
+        if idx == 0 {
+            directions = parse_instructions(line);
+            continue;
+        }
+        add_crossroad_to_map(&mut map, line);
     }
 
-    
+    let mut all_starting_nodes: Vec<String> = map
+        .clone()
+        .into_keys()
+        .filter(|k| k.ends_with("A"))
+        .collect();
+
+    let mut moves = 0;
+
+    for (i, d) in directions.iter().cycle().enumerate() {
+        let mut all_ending_nodes: Vec<_> = Vec::new();
+
+        moves += 1;
+        for n in &all_starting_nodes {
+            if let Some(coords) = map.get(n) {
+                let (left, right) = coords.clone();
+                match d {
+                    Direction::Left => match map.get(&left) {
+                        Some(_) => {
+                            all_ending_nodes.push(left.clone());
+                        }
+                        None => (),
+                    },
+                    Direction::Right => match map.get(&right) {
+                        Some(_) => {
+                            all_ending_nodes.push(right.clone());
+                        }
+                        None => (),
+                    },
+                }
+            }
+        }
+
+        if all_ending_nodes.iter().filter(|n| n.ends_with("Z")).count() == all_starting_nodes.len()
+        {
+            println!("All ending nodes found in {} steps. Exiting...", moves);
+            break;
+        }
+        if i % 1000 == 0 {
+            let mut show_it = true;
+            for n in &all_ending_nodes {
+                if n.ends_with("Z") {
+                    if show_it {
+                        println!("State after {} iterations", i);
+                        show_it = false;
+                    }
+                    println!("Node: {}", n);
+                }
+            }
+        }
+        all_starting_nodes = all_ending_nodes;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod aoc8;
 
 fn main() {
     aoc8::day08_task01();
+    aoc8::day08_task02();
 }
