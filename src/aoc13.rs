2688ff9330b260f2fb85da2a77714f4045d176e9
Dariusz D <darekduda21@gmail.com>
2024-10-25 14:51:11 -0400

    feat(aoc13): add solution for part 1
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc13.rs
+++ src/aoc13.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::Path;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Tile {
     ASH,
     ROCK,
@@ -9,8 +9,8 @@ enum Tile {
 
 #[derive(Debug)]
 enum Solution {
-    Vertical(u64),
-    Horizontal(u64),
+    Vertical { col: u64 },
+    Horizontal { row: u64 },
 }
 
 fn vec_from_str(s: &str) -> Vec<Tile> {
@@ -26,10 +26,10 @@ fn vec_from_str(s: &str) -> Vec<Tile> {
     tiles_row
 }
 
-fn compare_tiles_rows(tiles_row: &Vec<Tile>, other_tiles_row: &Vec<Tile>) -> bool {
+fn compare_tiles(tiles: &Vec<Tile>, cmp_tiles: &Vec<Tile>) -> bool {
     let mut equal = true;
-    for j in 0..tiles_row.len() {
-        if tiles_row[j] != other_tiles_row[j] {
+    for j in 0..tiles.len() {
+        if tiles[j] != cmp_tiles[j] {
             equal = false;
             break;
         }
@@ -38,22 +38,33 @@ fn compare_tiles_rows(tiles_row: &Vec<Tile>, other_tiles_row: &Vec<Tile>) -> boo
     equal
 }
 
-fn find_solution(tiles: &Vec<Vec<Tile>>) -> Solution {
-    let mut solution: Solution = Solution::Vertical(0);
+fn extract_column(tiles: &Vec<Vec<Tile>>, col: usize) -> Vec<Tile> {
+    let mut extracted_column = Vec::new();
+    for tile_row in tiles {
+        match tile_row.get(col) {
+            Some(tile) => extracted_column.push(tile.clone()),
+            None => (),
+        }
+    }
+    extracted_column
+}
 
-    let tiles_num = tiles.len();
-    for i in 1..tiles_num {
+fn find_solution(tiles: &Vec<Vec<Tile>>) -> Solution {
+    let mut solution: Solution = Solution::Vertical { col: 0 };
+
+    let tiles_rows = tiles.len();
+    for i in 1..tiles_rows {
         let tiles_row = tiles.get(i).unwrap();
         let prev_row = tiles.get(i - 1).unwrap();
-        let equal = compare_tiles_rows(&tiles_row, &prev_row);
+        let equal = compare_tiles(&tiles_row, &prev_row);
         if equal {
             let mut check = true;
+            // check if rest of the rows are also mirrored
             for k in (0..i - 1).rev() {
                 let difference = (i - k) + i - 1;
 
-                if difference < tiles_num {
-                    check &=
-                        compare_tiles_rows(&tiles.get(k).unwrap(), &tiles.get(difference).unwrap());
+                if difference < tiles_rows {
+                    check &= compare_tiles(&tiles.get(k).unwrap(), &tiles.get(difference).unwrap());
                 }
 
                 if !check {
@@ -61,13 +72,47 @@ fn find_solution(tiles: &Vec<Vec<Tile>>) -> Solution {
                 }
             }
             if check {
-                solution = Solution::Horizontal(i.try_into().unwrap());
+                solution = Solution::Horizontal {
+                    row: i.try_into().unwrap(),
+                };
             }
             println!(
                 "Found equal rows {}\n {:?}\n {:?}\n",
                 i, prev_row, tiles_row
             );
-            println!("Current solution: {:?}", solution,);
+        }
+    }
+
+    let tiles_columns = tiles.get(0).unwrap().len();
+    for i in 1..tiles_columns {
+        let tiles_column = extract_column(tiles, i);
+        let prev_col = extract_column(tiles, i - 1);
+        let equal = compare_tiles(&tiles_column, &prev_col);
+        if equal {
+            let mut check = true;
+            // check if rest of the rows are also mirrored
+            for k in (0..i - 1).rev() {
+                let difference = (i - k) + i - 1;
+                let left_hand_column = extract_column(tiles, k);
+
+                if difference < tiles_columns {
+                    let mirror_column = extract_column(tiles, difference);
+                    check &= compare_tiles(&left_hand_column, &mirror_column);
+                }
+
+                if !check {
+                    break;
+                }
+            }
+            if check {
+                solution = Solution::Vertical {
+                    col: i.try_into().unwrap(),
+                };
+            }
+            println!(
+                "Found equal columns {}\n {:?}\n {:?}\n",
+                i, prev_col, tiles_column
+            );
         }
     }
 
@@ -75,7 +120,7 @@ fn find_solution(tiles: &Vec<Vec<Tile>>) -> Solution {
 }
 
 fn solve(file_path: &Path) -> u64 {
-    let res = 0;
+    let mut res = 0;
     let file_content = fs::read_to_string(file_path).expect("File could not be loaded");
     let mut tiles: Vec<Vec<Tile>> = Vec::new();
     for line in file_content.split("\n") {
@@ -83,7 +128,14 @@ fn solve(file_path: &Path) -> u64 {
             for t in &tiles {
                 println!("Tilerow {:?}", t);
             }
-            find_solution(&tiles);
+            let solution = find_solution(&tiles);
+            println!("Current solution: {:?}", solution,);
+
+            res += match solution {
+                Solution::Vertical { col } => col,
+                Solution::Horizontal { row } => 100 * row,
+            };
+
             tiles.clear();
             continue;
         }
