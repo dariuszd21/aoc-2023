709021f28491da1d89b2430a00e1e7c3dae5fd8e
Dariusz D <darekduda21@gmail.com>
2024-12-03 00:15:52 -0500

    feat(aoc_15): add solution for part 2
    Signed-off-by: Dariusz D <darekduda21@gmail.com>

--- src/aoc15.rs
+++ src/aoc15.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 
@@ -36,10 +37,82 @@ pub fn solve_part_01(file_path: &Path) -> u64 {
 }
 
 pub fn solve_part_02(file_path: &Path) -> u64 {
-    let mut res = 0;
+    let mut res: u64 = 0;
     let sequence = load_sequence(file_path);
 
+    let mut hash_map: HashMap<u8, Vec<(String, u8)>> = HashMap::new();
+
+    for i in 0..=255 {
+        hash_map.insert(i, Vec::new());
+    }
+
     println!("\n\n----------\n\n");
+
+    for elem in sequence {
+        if elem.ends_with("-") {
+            if let Some(stripped_elem) = elem.strip_suffix("-") {
+                let hash = hashing_algorithm(stripped_elem);
+                let mut idx: Option<usize> = None;
+                match &mut hash_map.get_mut(&hash) {
+                    Some(vec) => {
+                        for (i, (name, _)) in vec.iter().enumerate() {
+                            if name == stripped_elem {
+                                idx = Some(i);
+                                break;
+                            }
+                        }
+                        match idx {
+                            Some(el_index) => {
+                                let (name, lens) = vec.remove(el_index);
+                                println!("Removing: {} {}", name, lens);
+                            }
+                            None => (),
+                        };
+                    }
+                    None => (),
+                }
+            }
+            continue;
+        }
+        let splitted_vec: Vec<_> = elem.split("=").collect();
+        if splitted_vec.len() == 2 {
+            let (name, lens_len) = (splitted_vec[0], splitted_vec[1].parse::<u8>().unwrap());
+            println!("{} {}", name, lens_len);
+            let hash = hashing_algorithm(name);
+            match &mut hash_map.get_mut(&hash) {
+                Some(vec) => {
+                    let mut idx: Option<usize> = None;
+                    for (i, (item_name, _)) in vec.iter().enumerate() {
+                        if name == item_name {
+                            idx = Some(i);
+                            break;
+                        }
+                    }
+                    match idx {
+                        Some(el_index) => {
+                            let (name, lens) = vec.remove(el_index);
+                            println!("Removing: {} {}", name, lens);
+                            println!("Inserting: {} {}", name, lens_len);
+                            vec.insert(el_index, (name.to_string(), lens_len));
+                        }
+                        None => {
+                            println!("Inserting: {} {}", name, lens_len);
+                            vec.push((name.to_string(), lens_len));
+                        }
+                    };
+                }
+                None => (),
+            }
+        }
+    }
+
+    for (box_nr, box_content) in hash_map {
+        println!("Analysing box: {}", box_nr as u64 + 1);
+        for (i, (_, lens_focal)) in box_content.iter().enumerate() {
+            res += (box_nr as u64 + 1) * (i as u64 + 1) * (*lens_focal as u64);
+        }
+        println!("Current res: {}", res);
+    }
 
     res
 }
@@ -66,7 +139,7 @@ mod tests {
             Ok(filepath) => filepath.join("input_d15_test"),
             Err(_) => panic!("Cannot find current directory"),
         };
-        assert_eq!(solve_part_01(&input_filepath), 136);
+        assert_eq!(solve_part_01(&input_filepath), 1320);
     }
 
     #[test]
@@ -75,6 +148,6 @@ mod tests {
             Ok(filepath) => filepath.join("input_d15_test"),
             Err(_) => panic!("Cannot find current directory"),
         };
-        assert_eq!(solve_part_02(&input_filepath), 64);
+        assert_eq!(solve_part_02(&input_filepath), 145);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-mod aoc14;
+mod aoc15;
 
 fn main() {
     let input_filepath = match std::env::current_dir() {
-        Ok(filepath) => filepath.join("input_d14_01"),
+        Ok(filepath) => filepath.join("input_d15_01"),
         Err(_) => panic!("Cannot find current directory"),
     };
-    let result = aoc14::solve_part_01(&input_filepath);
+    let result = aoc15::solve_part_01(&input_filepath);
     println!("And the result is {}", result);
 
-    let result = aoc14::solve_part_02(&input_filepath);
+    let result = aoc15::solve_part_02(&input_filepath);
     println!("And the result for part 2 is {}", result);
 }
